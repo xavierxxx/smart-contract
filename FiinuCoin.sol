767b11eec9f409ec04ced9e167372df1731c0ca8
Indrek Pari <indrek8@gmail.com>
2017-10-07 11:14:40 +0100

    Fix 7: ERC20 compliance
    Although a minor problem, the decimals state variable should be defined with type uint8 to be compliant with ERC20.
    Additionally, there is a restriction to the usage of approve intended to mitigate a problematic race condition, but it is not ERC20 compliant. Consider removing the line, and including an alternative mitigation.

--- FiinuCoin.sol
+++ FiinuCoin.sol
@@ -64,32 +64,52 @@ contract ERC20 {
 contract StandardToken is ERC20 {
     using SafeMath for uint;
     mapping(address => uint) balances;
-    mapping(address => mapping (address => uint)) allowed;
-    function transfer(address _to, uint _value) returns (bool) {
+    mapping(address => mapping (address => uint)) internal allowed;
+    function transfer(address _to, uint _value) public returns (bool) {
+        require(_to != address(0));
+        require(_value <= balances[msg.sender]);
+    
         balances[msg.sender] = balances[msg.sender].sub(_value);
         balances[_to] = balances[_to].add(_value);
         Transfer(msg.sender, _to, _value);
         return true;
     }
-    function balanceOf(address _owner) constant returns (uint balance) {
+    function balanceOf(address _owner) public constant returns (uint balance) {
         return balances[_owner];
     }
-    function transferFrom(address _from, address _to, uint _value) returns (bool) {
-        var _allowance = allowed[_from][msg.sender];
+    function transferFrom(address _from, address _to, uint _value) public returns (bool) {
+        require(_to != address(0));
+        require(_value <= balances[_from]);
+        require(_value <= allowed[_from][msg.sender]);
+    
         balances[_from] = balances[_from].sub(_value);
         balances[_to] = balances[_to].add(_value);
-        allowed[_from][msg.sender] = _allowance.sub(_value);
+        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
         Transfer(_from, _to, _value);
         return true;
     }
-    function approve(address _spender, uint _value) returns (bool) {
-        require((_value == 0) || (allowed[msg.sender][_spender] == 0));
+    function approve(address _spender, uint _value) public returns (bool) {
         allowed[msg.sender][_spender] = _value;
         Approval(msg.sender, _spender, _value);
         return true;
     }
-    function allowance(address _owner, address _spender) constant returns (uint remaining) {
+    function allowance(address _owner, address _spender) public constant returns (uint remaining) {
         return allowed[_owner][_spender];
+    }
+    function increaseApproval (address _spender, uint _addedValue) public returns (bool success) {
+        allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_addedValue);
+        Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+        return true;
+    }
+    function decreaseApproval (address _spender, uint _subtractedValue) public returns (bool success) {
+        uint oldValue = allowed[msg.sender][_spender];
+        if (_subtractedValue > oldValue) {
+            allowed[msg.sender][_spender] = 0;
+        } else {
+            allowed[msg.sender][_spender] = oldValue.sub(_subtractedValue);
+        }
+        Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+        return true;
     }
 }
 contract Ownable {
@@ -181,7 +201,7 @@ contract FiinuToken is StandardToken, Investors {
     address wallet;
     string public constant name = "Fiinucoin";
     string public constant symbol = "FNU";
-    uint public constant decimals = 6;
+    uint8 public constant decimals = 6;
     uint constant minRaiseWei = 20000 * 10 ** 18;
     uint constant targetRaiseWei = 100000 * 10 ** 18;
     uint constant maxRaiseWei = 400000 * 10 ** 18;
